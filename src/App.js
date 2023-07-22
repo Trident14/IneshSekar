8a17403bcaf10314ede41d122eb54fbe9fe20d75
Trident14 <sekarinesh14@gmail.com>
2023-07-22 15:59:16 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import  Navbar from './components/navbar.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainSection from './components/mainSection.js';
+import './Style/App.css'
+import Via from './via.js';
+
+
+
+const App = () => {
+  return (
+    <>    
+      <div>
+        <Router>
+          <Navbar />
+            <Routes>
+              <Route path='/' element={<MainSection /> } />
+              <Route path='/GridConquest' element={<Via LinkToPage={"/gridConquest"} /> } />
+              <Route path='/CoffeeMate'   element={<Via LinkToPage={"/CoffeeMate"}/>} />
+                
+            </Routes>
+          </Router>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "../Style/navbar.css"
+import logo1 from "../Asset/logo1.png"
+import { Link, useNavigate } from "react-router-dom";
+
+
+
+const Navbar = () => {
+  const Navigate=useNavigate()
+    const handleLogoClick = () => {
+      Navigate('/');
+    };
+
+
+  return (
+
+      <>
+      <div className="nav">
+        <a href="/" className="logo-wrapper">
+         <img src={logo1} loading="lazy" alt="Inesh Sekar" class="logo-name" onClick={handleLogoClick}/>
+        </a>
+         
+          <div className="nav-menu" id="nav-link">
+             <div className="nav-item-child">
+               <Link to="/" href="/">Home</Link>
+             </div>
+             <div className="nav-item-child">
+              <a href="#project">Project</a>
+             </div>
+             <div className="nav-item-child">
+             <a href="#contact">Contact</a>
+             </div>
+          </div>
+        </div>
+
+      </>
+    );
+  };
+
+export default Navbar
+
